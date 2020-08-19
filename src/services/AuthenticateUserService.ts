af8351339b8e9eb8c47fb9f4c7b8e5733d17b162
Juliana Grutzmann <juliana_grutzmann@hotmail.com>
2020-08-19 15:13:15 +1000

    add jwt token authentication

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,6 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
-// import { hash } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -11,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -32,9 +33,14 @@ class AuthenticateUserService {
     }
 
     // Usuario autenticado
+    const token = sign({}, 'af36083f2a8802cd9543caa67618fb58', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
 
     return {
       user,
+      token,
     };
   }
 }
